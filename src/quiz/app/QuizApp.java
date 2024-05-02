d1b8d8f5aea232ed7597f08ee947ed709fde80f6
2bodre3 <a.h.2bodre3@outlook.com>
2024-05-03 00:46:28 +0300

    make the main frame and add background image

--- src/quiz/app/QuizApp.java
+++ src/quiz/app/QuizApp.java
@@ -1,20 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package quiz.app;
 
-/**
- *
- * @author ah2bo
- */
-public class QuizApp {
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-    /**
-     * @param args the command line arguments
-     */
+public class QuizApp {
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        JFrame frame = new JFrame("Quiz-App");
+        ImageIcon background_image =new ImageIcon("D:\\projects\\java\\Quiz-app\\src\\image\\background.png");
+        Image img = background_image.getImage();
+        Image temp_img=img.getScaledInstance(1920, 1080, Image.SCALE_SMOOTH);
+        background_image = new ImageIcon(temp_img);
+        JLabel label = new JLabel(background_image);
+        frame.add(label);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
-    
 }
